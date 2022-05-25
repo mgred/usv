b8f7f3f774924698f868b3692a6bfa7406a5cfe8
Marc Redemske <marc.redemske@gmail.com>
2022-05-25 16:08:05 +0200

    chore: add cli package

---
+++ cli/index.ts
@@ -0,0 +1,79 @@
+import { existsSync, readFileSync } from "fs";
+import { argv, exit, stdout } from "process";
+import { PrintOptions, SeparatorOptions, transform } from "usv";
+
+const args = argv.slice(2);
+const options = {
+  finalNewline: undefined,
+  separators: {} as Partial<SeparatorOptions>,
+};
+let values = [];
+const cliOptions = new Map<TestFn, ActionFn>([
+  [test("-h", "--help"), printHelpAndExit],
+  [test("-u", "--unit"), readSeparator("unit")],
+  [test("-r", "--record"), readSeparator("record")],
+  [test("-g", "--group"), readSeparator("group")],
+  [test("-f", "--file"), readSeparator("file")],
+  [test("-n", "--newline"), () => (options.finalNewline = true)],
+  [test("--"), () => readFileToValue(0)],
+  [(f) => existsSync(f), readFile],
+  [() => true, printUnknownOption],
+]);
+
+let arg = "";
+args: while ((arg = args.shift()))
+  for (const [t, a] of cliOptions.entries())
+    if (t(arg) && (a(arg, args) || true)) continue args;
+
+for (const v of values) {
+  const result = transform(v, options as Partial<PrintOptions>);
+  stdout.write(result);
+}
+
+function printUnknownOption(option: string): void {
+  console.error(`Unknown option: '${option}'`);
+  exit(1);
+}
+
+function readFileToValue(file: string | number) {
+  values.push(readFileSync(file).toString());
+}
+
+function readFile(file: string): void {
+  try {
+    readFileToValue(file);
+  } catch (e) {
+    console.error(`Could not read file '${file}'\n`, e.message);
+    exit(1);
+  }
+}
+
+function readSeparator(name: string): ActionFn {
+  return (_, rest) => (options.separators[name] = rest.shift());
+}
+
+function test(...flags: string[]): TestFn {
+  return (value) => flags.some((f) => f === value);
+}
+
+function printHelpAndExit(): void {
+  stdout.write(`
+usv cli 0.0.0-PLACEHOLDER
+
+Usage: usv [OPTION]... [FILE]...
+
+Options:
+
+  -h, --help      Print help message
+  -u, --unit      Set unit separator
+  -r, --record    Set record separator
+  -g, --group     Set group separator
+  -f, --file      Set file separator
+  -n, --newline   Set final newline after output
+  --              Read from stdin
+`);
+  exit(0);
+}
+
+type TestFn = (value: string) => boolean;
+type ActionFn = (current: string, rest: string[]) => unknown;
